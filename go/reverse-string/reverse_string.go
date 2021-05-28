67542aba30b0dd18fc6c5a1c1c6344faa5d793d3
Daniel Theodoro <daniel.theodoro@gmail.com>
2021-05-28 13:51:21 +0200

    Update reverse_string to something more simple

--- go/reverse-string/reverse_string.go
+++ go/reverse-string/reverse_string.go
@@ -1,18 +1,10 @@
 package reverse
 
 func Reverse(s string) string {
-	word := make([]rune, len(s))
+	word := []rune(s)
 
-	count := 0
-	for _, i := range s {
-		word[count] = i
-		count++
-	}
-
-	word = word[0:count]
-
-	for i := 0; i < count/2; i++ {
-		word[i], word[count-1-i] = word[count-1-i], word[i]
+	for i := 0; i < len(word)/2; i++ {
+		word[len(word)-1-i], word[i] = word[i], word[len(word)-1-i]
 	}
 
 	return string(word)
